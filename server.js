7b96cafd6487be8d868114da909d2598e8fe8735
Ibrakhim Abdurachmanov <bleach_fanart@hotmail.com>
2019-02-01 08:53:42 +0100

    connection to mongodb

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+
+MongoClient.connect('mongodb://localhost:27017/examen', { useNewUrlParser: true },
+ (err, database) => {
+    if (err) return console.log(err)
+    db = database.db('examen')
+    app.listen(process.env.PORT || 4000, () => {
+      console.log('Listening on port 4000')
+    })
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
